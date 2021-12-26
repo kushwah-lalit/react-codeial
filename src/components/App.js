d8b24e23b7b8a0fca7a242831c6b2f8c068c6c6f
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-12-27 01:33:20 +0530

    Setup the directory structure for redux, added navbar, populated post and created corresponding action and reducer, added prop-types for props validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,85 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-     Hello
-    </div>
-  );
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://cdn-icons.flaticon.com/png/512/954/premium/954591.png?token=exp=1640549584~hmac=fa5ab01814a8b5d5b410bc3c6d7c4526"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1640549636~hmac=77fd2bde6123dc66516dc505cf69b662"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1640549636~hmac=77fd2bde6123dc66516dc505cf69b662"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1640549636~hmac=77fd2bde6123dc66516dc505cf69b662"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,5 +1,10 @@
-export default function posts(state = []) {
-    // {} or [] depends on our data
-    return state;
+import { UPDATE_POSTS } from '../actions/actionTypes';
+
+export default function posts(state = [], action) {
+  switch (action.type) {
+    case UPDATE_POSTS:
+      return action.posts;
+    default:
+      return state;
   }
-  
+}
