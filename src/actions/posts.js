2916bffc2b119bed552d265065bd528627ba752e
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2022-01-06 17:41:31 +0530

    Added refs to login form

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,7 +2,7 @@ import { UPDATE_POSTS } from './actionTypes';
 
 export function fetchPosts() {
   return (dispatch) => {
-    const url = 'http://codeial.codingninjas.com:8000/api/v2/posts?page=1&limit=5';
+    const url = 'https://codeial.codingninjas.com:8000/api/v2/posts?page=1&limit=5';
     fetch(url)
       .then((response) => {
         return response.json();
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Link, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
-import { PostsList, Navbar } from './';
-
-const Login = () => <div>Login</div>;
+import { Home, Navbar, Page404, Login } from './';
 
 const Signup = () => <div>Signup</div>;
-
-const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -23,24 +19,19 @@ class App extends React.Component {
       <Router>
         <div>
           <Navbar />
-          {/* <PostsList posts={posts} /> */}
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
-          {/* latest syntax */}
-          <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          </Routes>
+
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => {
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route component={Page404} />
+          </Switch>
         </div>
       </Router>
     );
