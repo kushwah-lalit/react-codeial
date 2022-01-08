e6e0c3dfcb38351f7edf7736b31660369226e215
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2022-01-08 20:58:48 +0530

    Added the friends list component, friend list items.....added the action and reducer to fetch the friends and made separte state for it....also passing the fetch friends as props to the friendlist via props...as we want to show the friend list on looged in only

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,4 +1,4 @@
-const API_ROOT = ' http://codeial.codingninjas.com:8000/api/v2';
+const API_ROOT = 'http://codeial.codingninjas.com:8000/api/v2';
 
 export const APIUrls = {
   login: () => `${API_ROOT}/users/login`,
@@ -7,4 +7,5 @@ export const APIUrls = {
   fetchPosts: (page = 1, limit = 5) =>
     `${API_ROOT}/posts?page=${page}&limit=${limit}`,
   userProfile: (userId) => `${API_ROOT}/users/${userId}`,
+  userFriends: () => `${API_ROOT}/friendship/fetch_user_friends`,
 };
