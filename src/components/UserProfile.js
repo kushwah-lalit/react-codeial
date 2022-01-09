d1911023f8ce6cac49df5c7cc2440d7b14358e52
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2022-01-09 11:58:15 +0530

    Added function to check whether the requested use is in our friend list...also showing btn accordingly

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -14,6 +14,16 @@ class UserProfile extends Component {
 
   checkIfUserIsAFriend = () => {
     console.log('this.props', this.props);
+    const { match, friends } = this.props;
+    const userId = match.params.userId;
+
+    const index = friends.map((friend) => friend.to_user._id).indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
   };
 
   render() {
@@ -27,6 +37,8 @@ class UserProfile extends Component {
     if (profile.inProgress) {
       return <h1>Loading!</h1>;
     }
+
+    const isUserAFriend = this.checkIfUserIsAFriend();
 
     return (
       <div className="settings">
@@ -48,16 +60,21 @@ class UserProfile extends Component {
         </div>
 
         <div className="btn-grp">
-          <button className="button save-btn">Add Friend</button>
+          {!isUserAFriend ? (
+            <button className="button save-btn">Add Friend</button>
+          ) : (
+            <button className="button save-btn">Remove Friend</button>
+          )}
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ profile }) {
+function mapStateToProps({ profile, friends }) {
   return {
     profile,
+    friends,
   };
 }
 export default connect(mapStateToProps)(UserProfile);
